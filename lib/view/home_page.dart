6c7b6f41443a49156b73bc58e17924a52a22bfdb
jassimpt <jassimmuthu990@gmail.com>
2024-03-27 00:27:30 +0530

    almost completed

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,86 +1,126 @@
+import 'package:employee_list/controller/employee_controller.dart';
+import 'package:employee_list/model/employee_model.dart';
 import 'package:employee_list/view/add_employe.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Provider.of<EmployeeController>(context, listen: false).fetchAllEmployees();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[600],
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => AddEmploye()));
-        },
-        child: const Icon(Icons.add),
-      ),
-      appBar: AppBar(
-        backgroundColor: Colors.blue[800],
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Employee ',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'detail',
-              style: TextStyle(
-                  color: Colors.orange,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
-            )
-          ],
+        backgroundColor: Colors.blue[600],
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => AddEmploye()));
+          },
+          child: const Icon(Icons.add),
         ),
-      ),
-      body: Container(
-        margin: const EdgeInsets.only(left: 20, right: 20, top: 30),
-        child: Column(
-          children: [
-            Material(
-              elevation: 5,
-              borderRadius: BorderRadius.circular(10),
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    color: Colors.blue[100],
-                    borderRadius: BorderRadius.circular(10)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Name : Hubaib',
-                      style: TextStyle(
-                          color: Colors.blue[800],
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Age : 21',
-                      style: TextStyle(
-                          color: Colors.orange[800],
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Location : Malappuram',
-                      style: TextStyle(
-                          color: Colors.blue[800],
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
+        appBar: AppBar(
+          backgroundColor: Colors.blue[800],
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Employee ',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
               ),
-            )
-          ],
+              Text(
+                'detail',
+                style: TextStyle(
+                    color: Colors.orange,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
         ),
-      ),
-    );
+        body: Consumer<EmployeeController>(
+          builder: (context, value, child) {
+            return ListView.builder(
+              itemCount: value.employeeList.length,
+              itemBuilder: (context, index) {
+                final EmployeeModel employee = value.employeeList[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Material(
+                    elevation: 5,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: Colors.blue[100],
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Name : ${employee.name}',
+                                style: TextStyle(
+                                    color: Colors.blue[800],
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                'Age : ${employee.age.toString()}',
+                                style: TextStyle(
+                                    color: Colors.orange[800],
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                'Location : ${employee.location}',
+                                style: TextStyle(
+                                    color: Colors.blue[800],
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.edit,
+                                    color: Colors.orange,
+                                  )),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.delete),
+                                color: Colors.red,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ));
   }
 }
